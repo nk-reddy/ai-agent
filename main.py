12427b061a1c72c410a297e0cef8f5fa73551582
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-06-27 21:28:09 -0400

    added file validation

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import os
+import argparse
+from google import genai
+from google.genai import types
+
+
+from dotenv import load_dotenv
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("no api key found")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+    args = parser.parse_args()
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    
+    if (response.usage_metadata is None):
+        raise Exception("failed api request")
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
